912fe9af96ad2c85620feb0e7c8999fea76dac2c
Mostafa Hassaan <mostafa.a.hassaan@gmail.com>
2016-06-28 21:11:39 +0200

    rtl

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( tm-scripts.js )
+Rails.application.config.assets.precompile += %w( application-ar.css )
