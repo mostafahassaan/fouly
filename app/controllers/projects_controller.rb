009ac4891b6c58cb1e719a88e288eee22b5dfe66
Mostafa Hassaan <mostafa.a.hassaan@gmail.com>
2016-08-16 11:21:08 +0200

    css fixes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   def index
     @projects = Project.where(:bigProject => false)
-    @bigProjects = Project.order("created_at DESC").page(params[:page]).per(4)
+    @bigProjects = Project.where(:bigProject => true).order("created_at DESC").page(params[:page]).per(4)
   end
 end
